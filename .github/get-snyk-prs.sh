5482639f8b6638500aea07f2ef599cc323782392
Misha Bruml <misha.bruml@kaluza.com>
2022-03-31 10:10:08 +0100

    remove linebreak

--- .github/get-snyk-prs.sh
+++ .github/get-snyk-prs.sh
@@ -1,2 +1,2 @@
-SUPERSNYK_PR_BODY=$(gh pr list --label snyk --json number | jq '.[]|.number' | while read line; do echo "closes #$line\n"; done)
-echo "{SUPERSNYK_PR_BODY}={$SUPERSNYK_PR_BODY}" >> $GITHUB_ENV
+SUPERSNYK_PR_BODY=$(gh pr list --label snyk --json number | jq '.[]|.number' | while read line; do echo "closes #$line"; done)
+echo "SUPERSNYK_PR_BODY=$SUPERSNYK_PR_BODY" >> $GITHUB_ENV
