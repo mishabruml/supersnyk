e1e18a92f216c77304f45252c95ce402f433fcbc
Misha Bruml <misha.bruml@kaluza.com>
2022-03-31 10:19:06 +0100

    test env syntax

--- .github/get-snyk-prs.sh
+++ .github/get-snyk-prs.sh
@@ -1,2 +1,2 @@
 SUPERSNYK_PR_BODY=$(gh pr list --label snyk --json number | jq '.[]|.number' | while read line; do echo "closes #$line"; done)
-echo "SUPERSNYK_PR_BODY=$SUPERSNYK_PR_BODY" >> $GITHUB_ENV
+echo "SUPERSNYK_PR_BODY=foo" >> $GITHUB_ENV
