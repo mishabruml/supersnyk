f5a020a84d52f7117199fc7000cdc69750366667
mishabruml <mishabruml@gmail.com>
2022-04-01 14:38:08 +0100

    inject branch names

--- create-supersnyk-branch.sh
+++ create-supersnyk-branch.sh
@@ -6,6 +6,6 @@ SUPERSNYK_TEMP_BRANCH=supersnyk_$(uuidgen)
 
 git branch $SUPERSNYK_TEMP_BRANCH $MAIN_BRANCH_NAME
 git checkout $SUPERSNYK_TEMP_BRANCH
-git pull origin ${{ env.SNYK_PR_BRANCH_NAMES }} --no-edit
+git pull origin $SNYK_PR_BRANCH_NAMES --no-edit
 git push origin $SUPERSNYK_TEMP_BRANCH
 echo "SUPERSNYK_TEMP_BRANCH="$SUPERSNYK_TEMP_BRANCH"" >> $GITHUB_ENV
